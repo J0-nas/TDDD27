a6fac6df41b38828b5966e3316cdca1c9d5b2619
Jonas Halstrup <jonashalstrup@Jonass-MacBook-Air.local>
2017-03-29 15:09:30 +0200

    extended react example

---
+++ dev_server/src/views/Content.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Content extends React.Component {
+  render() {
+    return <div>Content</div>;
+  }
+}
---
+++ dev_server/src/views/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Navbar extends React.Component {
+  render() {
+    return <div>NavBar</div>;
+  }
+}
