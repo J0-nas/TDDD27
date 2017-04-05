df97ae9cef33ec91075193d3b5b5301eac4ad380
Jonas Halstrup <jonashalstrup@Jonass-MacBook-Air.local>
2017-04-05 16:26:51 +0200

    added files and first definition of the structure

---
+++ dev_server/src/views/Standings.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Table } from 'react-bootstrap';
+
+export default class Standings extends React.Component {
+  render() {
+    return (
+      <Table responsive>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Points</th>
+        <th>Artist</th>
+        <th>Title</th>
+        <th>Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+    </tbody>
+  </Table>
+    );
+  }
+}
