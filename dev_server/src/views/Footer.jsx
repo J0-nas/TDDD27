5a1764af7341df3c0a13acbd13c862c15d839aa7
DoubleTheP <pamstal@yahoo.de>
2017-04-05 16:30:19 +0200

    tested react-bootstrap at header and footer

--- dev_server/src/views/Footer.jsx
+++ dev_server/src/views/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Navbar, Nav, NavItem, MenuItem, NavDropdown} from 'react-bootstrap';
+
 export default class Footer extends React.Component {
-  render() {
-    return (
-      <nav className="">
-        <a>Footer</a>
-      </nav>);
-  }
+    render() {
+        return (
+            <Navbar fixedBottom inverse>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        User
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav>
+                    <NavItem eventKey={1} href="#">Played: 12</NavItem>
+                    <NavItem eventKey={2} href="#">Won: 2</NavItem>
+                </Nav>
+            </Navbar>
+        );
+    }
 }
--- dev_server/src/views/mainView.jsx
+++ dev_server/src/views/mainView.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import NavBar from './NavBar.jsx';
+import Header from './Header.jsx';
 import Content from './Content.jsx';
 import Footer from './Footer.jsx';
 
 export default class MainView extends React.Component {
-  
+
 
   render() {
-    console.log("tesjhbt");
-    return  (<div id="MainView">
-              <NavBar/>
-              <Content/>
-              <Footer/>
-            </div>);
-  }
+    return  (
+        <div id="MainView">
+            <Header/>
+            <Content/>
+            <Footer/>
+        </div>
+        );
+    }
 }
 
 //module.exports = MainView;
