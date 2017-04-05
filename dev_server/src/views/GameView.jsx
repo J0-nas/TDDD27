6e71edd45529098e3c1a1121735d3b726b3827a6
Jonas Halstrup <jonashalstrup@Jonass-MacBook-Air.local>
2017-04-05 16:26:51 +0200

    added files and first definition of the structure

---
+++ dev_server/src/views/GameView.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class GameView extends React.Component {
+  render () {
+    return (
+      <h2>Game</h2>
+    );
+  }
+}
