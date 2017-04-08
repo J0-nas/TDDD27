7bbaded8c701fee33873928d8a23cd03930ac5bb
Jonas Halstrup <jonashalstrup@Jonass-MacBook-Air.local>
2017-04-08 15:35:45 +0200

    imported Krotos

--- mousika/mix.exs
+++ mousika/mix.exs
@@ -19,7 +19,9 @@ defmodule Mousika.Mixfile do
   def application do
     [mod: {Mousika, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :mariaex]]
+                    :phoenix_ecto, :mariaex, :httpoison
+                    ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +39,10 @@ defmodule Mousika.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+
+     {:krotos, git: "https://github.com/J0-nas/Krotos.git"}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
