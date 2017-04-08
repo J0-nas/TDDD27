0f83170fe53ab83820a04c08becca17a59e58723
Jonas Halstrup <jonashalstrup@Jonass-MacBook-Air.local>
2017-04-08 15:35:45 +0200

    imported Krotos

--- mousika/web/controllers/HelloController.ex
+++ mousika/web/controllers/HelloController.ex
@@ -1,11 +1,16 @@
 defmodule Mousika.HelloController do
   use Mousika.Web, :controller
+  import Krotos
 
   @static_file File.read!("priv/static/hello.html")
 
   def index(conn, _params) do
     #render conn, "index.html"
     IO.puts("Conn Path info:\t#{conn.path_info}")
-    html(conn, @static_file)
+    #html(conn, @static_file)
+    {name, artist, album, _, _} = Enum.at Krotos.getTopTracksForGenre("g.115"), 0
+    s = name <> " " <> artist <> " " <> album
+    IO.puts s
+    html(conn, s)
   end
 end
