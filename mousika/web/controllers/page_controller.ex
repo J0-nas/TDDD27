2c44b0e099026bfa5415a3819db3cf9abaa16041
Jonas Halstrup <jonas.halstrup@tu-harburg.de>
2017-05-01 00:37:24 +0200

    added changed based on stackoverflow and slack help

--- mousika/web/controllers/page_controller.ex
+++ mousika/web/controllers/page_controller.ex
@@ -1,14 +1,20 @@
 defmodule Mousika.PageController do
+  #import Plug.Conn
   use Mousika.Web, :controller
 
-  #@static_file File.read!("priv/static/index.html")
+  @static_file File.read!("priv/static/index.html")
   @index "/index.html"
+  @f_name (to_string System.cwd) <> "/priv/static/index.html"
 
   def index(conn, _params) do
-    #IO.puts("Conn Path info:\t#{conn.path_info}")
+    #IO.puts System.cwd()
+    #IO.puts __DIR__
+#IO.puts("Conn Path info:\t#{conn.path_info}")
     #IO.puts("in PC")
     #html(conn, @static_file)
     #html(conn, "test")
-    redirect(conn, to: @index)
+    #redirect(conn, to: @index)
+    send_resp(conn, 200, @static_file)
+    #Plug.Conn.send_file(conn, 200, @f_name)
   end
 end
