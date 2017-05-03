5ede2fae0d19a0fd542a41abc49c566546dbed87
Jonas Halstrup <jonas.halstrup@tu-harburg.de>
2017-05-03 19:05:05 +0200

    changed port to 80

--- mousika/config/prod.exs
+++ mousika/config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 # {:system, "PORT"}
 config :mousika, Mousika.Endpoint,
-  http: [port: 4000],
+  http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
