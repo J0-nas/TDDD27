b3be37a86dfa75c10bea33b619beb9b6e94e8bb5
Jonas Halstrup <jonas.halstrup@tu-harburg.de>
2017-06-04 09:55:06 +0200

    fixed resetting scores

--- dev_server/src/views/Standings.jsx
+++ dev_server/src/views/Standings.jsx
@@ -7,18 +7,49 @@ export default class Standings extends React.Component {
   constructor() {
     super();
     this.state = {
-      scores: []
+      scores: [],
+      rows: null,
     }
 
+    this.buildTableRows = this.buildTableRows.bind(this);
     this.handleNewScore = this.handleNewScore.bind(this);
     this.resetSolved = this.resetSolved.bind(this);
   }
 
+  buildTableRows(newScores) {
+    const newRows = newScores.map( (x, i) =>
+      {
+        var as = <Glyphicon glyph='remove'/>;
+        var ts = <Glyphicon glyph='remove'/>;
+        if (x.artistSolved) {
+          as = <Glyphicon glyph='ok'/>
+        }
+        if (x.titleSolved) {
+          ts = <Glyphicon glyph='ok'/>
+        }
+
+        return (
+          <tr>
+            <td>{i+1}</td>
+            <td>{x.username}</td>
+            <td>{x.points}</td>
+            <td>{as}</td>
+            <td>{ts}</td>
+            <td>{x.time} ms</td>
+            <th className="hidden-md hidden-ms hidden-xs"></th>
+          </tr>
+        )
+      }
+    )
+    this.setState({rows: newRows});
+  }
+
   resetSolved() {
-    var state = this.state.scores;
-    state = state.map(x => { return {username: x.username, points: x.points, artistSolved: false, titleSolved: false, time: 0}; } );
-    this.setState(state);
-    console.log("Standings state ", state);
+    var newState = this.state.scores;
+    newState = newState.map(x => { return {username: x.username, points: x.points, artistSolved: false, titleSolved: false, time: 0}; } );
+    this.setState({scores: newState});
+    this.buildTableRows(newState);
+    console.log("Standings state ", newState);
   }
 
   handleNewScore(value) {
@@ -28,14 +59,16 @@ export default class Standings extends React.Component {
       if (state[i].username == username) {
         state[i] = value;
         state.sort((x, y) => x.points > x.points);
-        this.setState(state);
+        this.setState({scores: state});
+        this.buildTableRows(state);
         console.log("Standings state ", state);
         return;
       }
     }
     state.push(value);
     state.sort((x, y) => x.points > x.points);
-    this.setState(state);
+    this.setState({scores: state});
+    this.buildTableRows(state);
     console.log("Standings state ", state);
   }
 
@@ -59,30 +92,7 @@ export default class Standings extends React.Component {
     </thead>
     <tbody>
       {
-        this.state.scores.map( (x, i) =>
-          {
-            var as = <Glyphicon glyph='remove'/>;
-            var ts = <Glyphicon glyph='remove'/>;
-            if (x.artistSolved) {
-              as = <Glyphicon glyph='ok'/>
-            }
-            if (x.titleSolved) {
-              ts = <Glyphicon glyph='ok'/>
-            }
-
-            return (
-              <tr>
-                <td>{i+1}</td>
-                <td>{x.username}</td>
-                <td>{x.points}</td>
-                <td>{as}</td>
-                <td>{ts}</td>
-                <td>{x.time} ms</td>
-                <th className="hidden-md hidden-ms hidden-xs"></th>
-              </tr>
-            )
-          }
-        )
+        this.state.rows
       }
     </tbody>
   </Table>
